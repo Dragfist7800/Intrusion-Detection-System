ed0c0fba5919192e307dbce7c7c2cff58a3f2a8b
Dayanand <danishboss022@gmail.com>
2022-04-01 22:54:19 +0530

    Added Comments

--- Raspberry Pi  config/scp.sh
+++ Raspberry Pi  config/scp.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 FILE=$(date +"%Y-%m-%d-%H-%M")
-sshpass -p 'your password' scp -r /home/pi/webcampics/*  Administrator@<'your instance ip here'>:c:/webcampics && rm -rf /home/pi/webcampics/*
+sshpass -p 'enter your password here' scp -r /home/pi/webcampics/*  Administrator@<enter your instance ip>:c:/webcampics && rm -rf /home/pi/webcampics/*
+
+#sshpass stores the password of ssh session and scp is used to send files to windows VM
+#all file locations or path specified can be changed as per wish
